88f57c03f050c8790aaaf3c22d47a19c671167ed
UncleRoderick <UncleRoderick@hotmail.com>
2021-11-28 11:14:48 +0800

    1.Add LVGL Ver 8.0.2.

--- src/sample_vio_main.c
+++ src/sample_vio_main.c
@@ -13,11 +13,31 @@ extern "C" {
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <time.h>
+#include <sys/time.h>
 #include <sys/ioctl.h>
-
+#include <linux/fb.h>
+#include "sample_comm.h"
 #include "hi_common.h"
 #include "sample_vio.h"
 #include "mpi_sys.h"
+#include "hifb.h"
+#include "loadbmp.h"
+#include "hi_tde_api.h"
+#include "hi_tde_type.h"
+#include "hi_tde_errcode.h"
+#include "hi_math.h"
+#include "loadbmp.h"
+#include "lvgl/lvgl.h"
+#include "lv_drivers/display/fbdev.h"
+#include "lv_demos/lv_demo.h"
+
+#include "lv_drivers/indev/mouse.h"
+
+#define WIDTH			1920
+#define HEIGHT			1080
+#define DISP_BUF_SIZE 	(128 * 1024)
+
 /******************************************************************************
 * function : show usage
 ******************************************************************************/
@@ -25,22 +45,125 @@ void SAMPLE_VIO_Usage(char *sPrgNm)
 {
     printf("Usage : %s <index>\n", sPrgNm);
     printf("index:\n");
-    printf("\t 0)VI (Online) - VPSS(Online) - VO.\n");
-    printf("\t 1)WDR(Offline)- VPSS(Offline) - VO. LDC+DIS+SPREAD.\n");
-    printf("\t 2)Resolute Ratio Switch.\n");
-    printf("\t 3)GDC - VPSS LowDelay.\n");
-    printf("\t 4)Double WDR Pipe.\n");
-    printf("\t 5)FPN Calibrate & Correction.\n");
-    printf("\t 6)WDR Switch.\n");
-    printf("\t 7)90/180/270/0/free Rotate.\n");
-    printf("\t 8)UserPic.\n");
-    printf("\t 9)VI-VPSS-VO(MIPI_TX).\n\n");
-
-    printf("\t Hi3516DV300/Hi3559V200/Hi3556V200) vo HDMI output.\n");
-    printf("\t Hi3516CV500) vo BT1120 output.\n");
+    printf("\t 0)LVGL HiFB Sample.\n");
+    printf("\t 1)GDC - VPSS LowDelay.\n");
+    printf("\t 2)FPN Calibrate & Correction.\n");
+    printf("\t 3)WDR Switch.\n");
+    printf("\t 4)90/180/270/0/free Rotate.\n");
+    printf("\t 5)VI-VPSS-VO(MIPI_TX).\n\n");
     printf("\t If you have any questions, please look at readme.txt!\n");
     return;
 }
+
+/*Set in lv_conf.h as `LV_TICK_CUSTOM_SYS_TIME_EXPR`*/
+uint32_t custom_tick_get(void)
+{
+    static uint64_t start_ms = 0;
+    if(start_ms == 0) {
+        struct timeval tv_start;
+        gettimeofday(&tv_start, NULL);
+        start_ms = (tv_start.tv_sec * 1000000 + tv_start.tv_usec) / 1000;
+    }
+
+    struct timeval tv_now;
+    gettimeofday(&tv_now, NULL);
+    uint64_t now_ms;
+    now_ms = (tv_now.tv_sec * 1000000 + tv_now.tv_usec) / 1000;
+
+    uint32_t time_ms = now_ms - start_ms;
+    return time_ms;
+}
+
+HI_S32 SAMPLE_HIFB_LVGL(HI_VOID)
+{	
+	HI_S32                   s32Ret        = HI_SUCCESS;
+    VB_CONFIG_S              stVbConf;
+	SAMPLE_VO_CONFIG_S		stVoDevInfo;
+
+	stVoDevInfo.VoDev = SAMPLE_VO_DEV_UHD;
+	stVoDevInfo.enVoIntfType = VO_INTF_HDMI;
+    stVoDevInfo.enIntfSync        = VO_OUTPUT_1080P60;
+    stVoDevInfo.u32BgColor        = COLOR_RGB_BLUE;
+    stVoDevInfo.enPixFormat       = PIXEL_FORMAT_YVU_SEMIPLANAR_420;
+    stVoDevInfo.stDispRect.s32X        = 0;
+    stVoDevInfo.stDispRect.s32Y        = 0;
+    stVoDevInfo.stDispRect.u32Width        = WIDTH;
+    stVoDevInfo.stDispRect.u32Height        = HEIGHT;
+    stVoDevInfo.stImageSize.u32Width       = WIDTH;
+    stVoDevInfo.stImageSize.u32Height       = HEIGHT;
+    stVoDevInfo.enVoPartMode      = VO_PART_MODE_SINGLE;
+    stVoDevInfo.u32DisBufLen      = 3;
+    stVoDevInfo.enDstDynamicRange = DYNAMIC_RANGE_SDR8;
+	stVoDevInfo.enVoMode          = VO_MODE_1MUX;
+
+    /******************************************
+     step  1: init variable
+    ******************************************/
+    memset(&stVbConf, 0, sizeof(VB_CONFIG_S));
+
+    /******************************************
+     step 2: mpp system init.
+    ******************************************/
+    s32Ret = SAMPLE_COMM_SYS_Init(&stVbConf);
+    if (HI_SUCCESS != s32Ret)
+    {
+        SAMPLE_PRT("system init failed with %d!\n", s32Ret);
+        goto SAMPLE_HIFB_StandarMode_0;
+    }
+
+    /******************************************
+     step 3: Start VO device.
+     NOTE: Step 3 is optional when VO is running on other system.
+    ******************************************/
+    s32Ret = SAMPLE_COMM_VO_StartVO(&stVoDevInfo);
+    if (HI_SUCCESS != s32Ret)
+    {
+        SAMPLE_PRT("SAMPLE_COMM_VO_StartVO failed with %d!\n", s32Ret);
+        goto SAMPLE_HIFB_StandarMode_0;
+    }
+
+    /******************************************
+     step 4:  start hifb.
+    *****************************************/
+	/*LittlevGL init*/
+	lv_init();
+	
+	/*Linux frame buffer device init*/
+	fbdev_init();
+	
+	/*A small buffer for LittlevGL to draw the screen's content*/
+	static lv_color_t buf[DISP_BUF_SIZE];
+	
+	/*Initialize a descriptor for the buffer*/
+	static lv_disp_draw_buf_t disp_buf;
+	lv_disp_draw_buf_init(&disp_buf, buf, NULL, DISP_BUF_SIZE);
+	
+	/*Initialize and register a display driver*/
+	static lv_disp_drv_t disp_drv;
+	lv_disp_drv_init(&disp_drv);
+	disp_drv.draw_buf	= &disp_buf;
+	disp_drv.flush_cb	= fbdev_flush;
+	disp_drv.hor_res	= WIDTH;
+	disp_drv.ver_res	= HEIGHT;
+	lv_disp_drv_register(&disp_drv);
+	
+	/*Create a Demo*/
+	//lv_demo_widgets();
+	lv_demo_benchmark();
+	
+	/*Handle LitlevGL tasks (tickless mode)*/
+	while(1) {
+		lv_task_handler();
+		usleep(5000);
+	}
+	
+	SAMPLE_COMM_VO_StopVO(&stVoDevInfo);
+SAMPLE_HIFB_StandarMode_0:
+	SAMPLE_COMM_SYS_Exit();
+	
+	return s32Ret;
+}
+
 
 /******************************************************************************
 * function    : main()
@@ -52,82 +175,43 @@ int app_main(int argc, char *argv[])
 int main(int argc, char *argv[])
 #endif
 {
+	HI_S32 s32Index;
     HI_S32 s32Ret = HI_FAILURE;
-    HI_S32 s32Index;
-    HI_U32 u32VoIntfType = 0;
-    HI_U32  u32ChipId;
+	HI_U32 u32VoIntfType = 0;
 
     if (argc < 2 || argc > 2)
     {
         SAMPLE_VIO_Usage(argv[0]);
         return HI_FAILURE;
     }
-
-    if (!strncmp(argv[1], "-h", 2))
-    {
-        SAMPLE_VIO_Usage(argv[0]);
-        return HI_SUCCESS;
-    }
-
-#ifdef __HuaweiLite__
-#else
+	
     signal(SIGINT, SAMPLE_VIO_HandleSig);
     signal(SIGTERM, SAMPLE_VIO_HandleSig);
-#endif
-
-    HI_MPI_SYS_GetChipId(&u32ChipId);
-
-    if (HI3516C_V500 == u32ChipId)
-    {
-        u32VoIntfType = 1;
-    }
-    else
-    {
-        u32VoIntfType = 0;
-    }
-
-    SAMPLE_VIO_MsgInit();
 
     s32Index = atoi(argv[1]);
     switch (s32Index)
     {
         case 0:
-            s32Ret = SAMPLE_VIO_ViOnlineVpssOnlineRoute(u32VoIntfType);
+            s32Ret = SAMPLE_HIFB_LVGL();
             break;
-
+		
         case 1:
-            s32Ret = SAMPLE_VIO_WDR_LDC_DIS_SPREAD(u32VoIntfType);
-            break;
-
-        case 2:
-            s32Ret = SAMPLE_VIO_ResoSwitch(u32VoIntfType);
-            break;
-
-        case 3:
             s32Ret = SAMPLE_VIO_ViVpssLowDelay(u32VoIntfType);
             break;
 
-        case 4:
-            s32Ret = SAMPLE_VIO_ViDoubleWdrPipe(u32VoIntfType);
-            break;
-
-        case 5:
+        case 2:
             s32Ret = SAMPLE_VIO_FPN(u32VoIntfType);
             break;
 
-        case 6:
+        case 3:
             s32Ret = SAMPLE_VIO_ViWdrSwitch(u32VoIntfType);
             break;
 
-        case 7:
+        case 4:
             s32Ret = SAMPLE_VIO_Rotate(u32VoIntfType);
             break;
 
-        case 8:
-            s32Ret = SAMPLE_VIO_SetUsrPic(u32VoIntfType);
-            break;
-
-        case 9:
+        case 5:
             s32Ret = SAMPLE_VIO_VPSS_VO_MIPI_TX(u32VoIntfType);
             break;
 
